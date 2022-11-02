3d96b09c5762d699d3f1e321450281adb2e7a4d4
Hellboycc <luoyingchuan1210@163.com>
2022-11-03 00:01:54 +0800

    feat(#3):implement functionality of ping check

--- netbox/netbox.py
+++ netbox/netbox.py
@@ -1,6 +1,29 @@
 import os
 
+import ping3
 
-def get_current_os_info():
 
-    return "Linux" if os.name == "posix" else "Windows"
+class Netbox(object):
+    def __init__(self) -> None:
+        self.ping = ping3.ping
+
+    def check_host_state(self, host, src_addr=None, interface=None):
+
+        name = Netbox._get_current_os_info()
+        if name == "Windows":
+            return self.ping(host=host, src_addr=src_addr)
+
+        elif name == "Linux":
+            return self.ping(host=host, interface=interface)
+        else:
+            return self.ping(host)
+
+    @staticmethod
+    def _get_current_os_info():
+
+        if os.name == "posix" and os.uname().sysname == "Darwin":
+            return "MacOS"
+        elif os.name == "posix":
+            return "Linux"
+        else:
+            return "Windows"
